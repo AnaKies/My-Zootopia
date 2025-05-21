b34b086af47f4de47985fcc56bca25ac854b55d6
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-21 15:29:33 +0200

    generate card item for every animal

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 import json
 
-from bs4 import BeautifulSoup
-
 
 def load_data(file_path):
     """
-    Loads json file.
-    :param file_path:
-    :return:
+    Loads JSON file.
+    :param file_path: path to the JSON file
+    :return: JSON data as string
     """
-    with open(file_path, "r") as json_file:
-        return json.load(json_file)
+    try:
+        with open(file_path, "r") as json_file:
+            return json.load(json_file)
+
+    except FileNotFoundError as error:
+        print(f"Error: File {file_path} is not found.\n{error}")
+    except json.JSONDecodeError as error:
+        print(f"Error: JSON {file_path} is not valid.\n{error}")
+    except Exception as error:
+        print(f"Error: Unexpected error at reading {file_path}.\n{error}")
+    return None
 
 
 def get_string_with_animals(animals_data):
@@ -22,26 +29,30 @@ def get_string_with_animals(animals_data):
     output_animals_data = ""
 
     for animal in animals_data:
-        characteristic_is_there = False # spare one if check later
+        # spare one if check later
+        characteristic_is_there = False
+
+        # append information to each string
+        output_animals_data += "<li class='cards__item'>"
 
         if "name" in animal.keys():
-            output_animals_data += f"Name: {animal['name']}\n"
+            output_animals_data += f"Name: {animal['name']}<br/>\n"
 
         if "characteristics" in animal.keys():
             characteristic_is_there = True
             if "diet" in animal["characteristics"]:
                 diet = animal["characteristics"]["diet"]
-                output_animals_data += f"Diet: {diet}\n"
+                output_animals_data += f"Diet: {diet}<br/>\n"
 
         if "locations" in animal.keys():
             first_location = animal["locations"][0]
-            output_animals_data += f"Locations: {first_location}\n"
+            output_animals_data += f"Locations: {first_location}<br/>\n"
 
         if characteristic_is_there:
             if "type" in animal["characteristics"]:
                 type_of_animal = animal["characteristics"]["type"]
-                output_animals_data += f"Type: {type_of_animal}\n"
-
+                output_animals_data += f"Type: {type_of_animal}<br/>\n"
+        output_animals_data += "</li>"
         output_animals_data += "\n"
 
     return output_animals_data
@@ -52,9 +63,18 @@ def get_html():
     Reads the content of the HTML file.
     :return: Content of the HTML file.
     """
-    with open("animals_template.html", "r") as html_file:
-        return html_file.read()
+    try:
+        file_name = "animals_template.html"
+        with open(file_name, "r") as html_file:
+            return html_file.read()
 
+    except FileNotFoundError as error:
+        print(f"Error: File {file_name} is not found.\n{error}")
+    except json.JSONDecodeError as error:
+        print(f"Error: JSON {file_name} is not valid.\n{error}")
+    except Exception as error:
+        print(f"Error: Unexpected error at reading {file_name}.\n{error}")
+    return None
 
 def write_html(html_content):
     """
@@ -62,16 +82,46 @@ def write_html(html_content):
     :param html_content: HTML content of the HTML file as string.
     :return: None
     """
-    with open("animals_template.html", "w") as html_file:
-        html_file.write(html_content)
+    try:
+        file_name = "animals.html"
+        with open(file_name, "w") as html_file:
+            html_file.write(html_content)
+
+    except FileNotFoundError as error:
+        print(f"Error: File {file_name} is not found.\n{error}")
+    except json.JSONDecodeError as error:
+        print(f"Error: JSON {file_name} is not valid.\n{error}")
+    except Exception as error:
+        print(f"Error: Unexpected error at writing {file_name}.\n{error}")
+
+
+def replace_html_with_animals(html_text, string_with_animals):
+    """
+    Replaces HTML content with animal data.
+    :param html_text: The HTML content of the HTML file as string.
+    :param string_with_animals: Text to be replaced with the animal data.
+    :return: string with replaced HTML content.
+    """
+    if "__REPLACE_ANIMALS_INFO__" in html_text:
+        html_text_replaced = html_text.replace(
+                                    "__REPLACE_ANIMALS_INFO__",
+                                    string_with_animals)
+        return html_text_replaced
+    else:
+        raise Exception("Error: the __REPLACE_ANIMALS_INFO__ is not in the template.")
 
 
 def main():
-    animals_data = load_data("animals_data.json")
-    string_with_animals = get_string_with_animals(animals_data)
-    html_text = get_html()
-    html_text_replaced = html_text.replace("__REPLACE_ANIMALS_INFO__", string_with_animals)
-    write_html(html_text_replaced)
+    try:
+        animals_data = load_data("animals_data.json")
+        string_with_animals = get_string_with_animals(animals_data)
+        html_text = get_html()
+        working_html_text = html_text # work with a copy and don't touch the original
+        html_text_replaced = replace_html_with_animals(working_html_text, string_with_animals)
+        write_html(html_text_replaced)
+    except Exception as error:
+        print(f"Unexpected error: {error}")
+
 
 if __name__ == "__main__":
     main()
