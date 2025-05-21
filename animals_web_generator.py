037ee6408878cfb99b241588535ff8bf6f8716fb
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-22 00:21:55 +0200

    Reformat code and adapt it to PEP8.

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -8,8 +8,9 @@ def load_data(file_path):
     :return: JSON data as string
     """
     try:
-        # use encoding for correct representation of such special symbols like an apostrophe
-        with open(file_path, "r", encoding = "utf-8") as json_file:
+        # use encoding for correct representation of
+        # such special symbols like an apostrophe
+        with open(file_path, "r", encoding="utf-8") as json_file:
             return json.load(json_file)
 
     except FileNotFoundError as error:
@@ -33,8 +34,8 @@ def serialize_animal(animal_obj):
     characteristic_is_there = False
 
     # append information to each string
-    output_animal_data += "<li class='cards__item'>\n"
-    output_animal_data += "<div class='card__title'>"
+    output_animal_data += ("<li class='cards__item'>\n"
+                           "<div class='card__title'>")
 
     if "name" in animal_obj.keys():
         output_animal_data += f"{animal_obj['name']}"
@@ -46,39 +47,58 @@ def serialize_animal(animal_obj):
 
     output_animal_data += "</div>\n"
 
-    output_animal_data += "<div class='card__text'><ul class='animal_list'>"
+    output_animal_data += ("<div class='card__text'>"
+                           "<ul class='animal_list'>")
     if "characteristics" in animal_obj.keys():
         characteristic_is_there = True
         if "diet" in animal_obj["characteristics"].keys():
             diet = animal_obj["characteristics"]["diet"]
-            output_animal_data += "<li class='animal_properties'><strong>Diet: </strong>"
+            output_animal_data += ("<li class='animal_properties'>"
+                                   "<strong>"
+                                   "Diet: "
+                                   "</strong>")
             output_animal_data += f"{diet}</li>\n"
 
     if "locations" in animal_obj.keys():
         if animal_obj["locations"]:  # check if the list is not empty
             first_location = animal_obj["locations"][0]
-            output_animal_data += "<li class='animal_properties'><strong>Location: </strong>"
+            output_animal_data += ("<li class='animal_properties'>"
+                                   "<strong>"
+                                   "Location: "
+                                   "</strong>")
             output_animal_data += f"{first_location}</li>\n"
 
     if characteristic_is_there:
         if "type" in animal_obj["characteristics"]:
             type_of_animal = animal_obj["characteristics"]["type"]
-            output_animal_data += "<li class='animal_properties'><strong>Type: </strong>"
+            output_animal_data += ("<li class='animal_properties'>"
+                                   "<strong>"
+                                   "Type: "
+                                   "</strong>")
             output_animal_data += f"{type_of_animal}</li>\n"
 
         if "lifespan" in animal_obj["characteristics"]:
             lifespan = animal_obj["characteristics"]["lifespan"]
-            output_animal_data += "<li class='animal_properties'><strong>Lifespan: </strong>"
+            output_animal_data += ("<li class='animal_properties'>"
+                                   "<strong>"
+                                   "Lifespan: "
+                                   "</strong>")
             output_animal_data += f"{lifespan}</li>\n"
 
         if "skin_type" in animal_obj["characteristics"]:
             lifespan = animal_obj["characteristics"]["skin_type"]
-            output_animal_data += "<li class='animal_properties'><strong>Skin type: </strong>"
+            output_animal_data += ("<li class='animal_properties'>"
+                                   "<strong>"
+                                   "Skin type: "
+                                   "</strong>")
             output_animal_data += f"{lifespan}</li>\n"
 
-    output_animal_data += "</ul></div></li>\n"
+    output_animal_data += ("</ul>"
+                           "</div>"
+                           "</li>\n")
 
     return output_animal_data
+
 
 def serialize_all_animals(animals_data):
     """
@@ -106,8 +126,10 @@ def get_unique_skin_types(animals_data):
         if "characteristics" in animal.keys():
             if "skin_type" in animal["characteristics"].keys():
                 # create a list with unique skin types
-                if animal["characteristics"]["skin_type"] not in unique_skin_types:
-                    unique_skin_types.append(animal["characteristics"]["skin_type"])
+                if (animal["characteristics"]["skin_type"]
+                        not in unique_skin_types):
+                    unique_skin_types.append(
+                        animal["characteristics"]["skin_type"])
 
     return unique_skin_types
 
@@ -119,8 +141,9 @@ def get_template_html():
     """
     try:
         file_name = "animals_template.html"
-        # use encoding for correct representation of such special symbols like an apostrophe
-        with open(file_name, "r", encoding = "utf-8") as html_file:
+        # use encoding for correct representation of
+        # such special symbols like an apostrophe
+        with open(file_name, "r", encoding="utf-8") as html_file:
             return html_file.read()
 
     except FileNotFoundError as error:
@@ -140,8 +163,9 @@ def create_html_file(file_name, html_content):
     :return: None
     """
     try:
-        # use encoding for correct representation of such special symbols like an apostrophe
-        with open(file_name, "w", encoding = "utf-8") as html_file:
+        # use encoding for correct representation of
+        # such special symbols like an apostrophe
+        with open(file_name, "w", encoding="utf-8") as html_file:
             html_file.write(html_content)
 
     except FileNotFoundError as error:
@@ -159,13 +183,15 @@ def insert_new_data_in_html_template(original_html, new_text):
     :param new_text: Text for replacement.
     :return: New HTML content with replaced text.
     """
-    if "__REPLACE_ANIMALS_INFO__" in original_html:
-        html_text_replaced = original_html.replace(
-                                    "__REPLACE_ANIMALS_INFO__",
-                                    new_text)
-        return html_text_replaced
-    else:
-        raise Exception("Error: the __REPLACE_ANIMALS_INFO__ is not in the template.")
+    if "__REPLACE_ANIMALS_INFO__" not in original_html:
+        raise Exception("Error: the "
+                        "__REPLACE_ANIMALS_INFO__ "
+                        "is not in the template.")
+
+    html_text_replaced = original_html.replace(
+        "__REPLACE_ANIMALS_INFO__",
+        new_text)
+    return html_text_replaced
 
 
 def collect_animals_with_selected_skin_type(skin_type, animals_data):
@@ -193,7 +219,8 @@ def collect_animals_with_selected_skin_type(skin_type, animals_data):
 
 def print_skin_types(unique_skin_types):
     """
-    Displays the available skin types or an error message if no skin types are available.
+    Displays the available skin types or an error message
+    if no skin types are available.
     :param unique_skin_types:
     :return: None
     """
@@ -215,7 +242,7 @@ def get_user_input(unique_skin_types):
     while True:
         selected_skin_type = input("Select a skin type to use: ")
         if selected_skin_type in unique_skin_types:
-            break;
+            break
         print("Invalid skin type.")
 
     return selected_skin_type
@@ -230,12 +257,18 @@ def print_animals_without_skin_type(counter_animals_without_skin_type):
     :return:
     """
     if counter_animals_without_skin_type > 0:
-        print(f"Attention! {counter_animals_without_skin_type} animals are not "
-              f"included in the animals data "
+        print(f"Attention! "
+              f"{counter_animals_without_skin_type} "
+              f"animals are not included in the animals data "
               f"because of missing skin type data.")
 
 
 def main():
+    """
+    The user chooses a skin type for an animal from the
+    list of available skin types. Then the HTML is generated containing
+    all the animals with the selected skin type.
+    """
     try:
         # get JSON data
         animals_data = load_data("animals_data.json")
@@ -245,17 +278,20 @@ def main():
         print_skin_types(unique_skin_types)
         selected_skin_type = get_user_input(unique_skin_types)
         animals_with_selected_skin, counter_animals_without_skin_type = (
-            collect_animals_with_selected_skin_type(selected_skin_type, animals_data))
+            collect_animals_with_selected_skin_type(
+                selected_skin_type, animals_data))
         print_animals_without_skin_type(counter_animals_without_skin_type)
 
         # manage the HTML
-        html_formatted_animals = serialize_all_animals(animals_with_selected_skin)
+        html_formatted_animals = serialize_all_animals(
+            animals_with_selected_skin)
         template_html_for_animals = get_template_html()
         full_html_with_animals = insert_new_data_in_html_template(
             template_html_for_animals, html_formatted_animals)
         file_name = "animals.html"
         create_html_file(file_name, full_html_with_animals)
-        print(f"The website {file_name} was generated for animals with selected skin type.")
+        print(f"The website {file_name} was generated for "
+              f"animals with selected skin type.")
 
     except Exception as error:
         print(f"Unexpected error: {error}")
