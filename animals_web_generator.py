537a7752adb4178562fdb9d96814cbbb44543ae5
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-22 00:04:50 +0200

    Orginize the code structure by wrapping some logic in methods.

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -168,9 +168,9 @@ def insert_new_data_in_html_template(original_html, new_text):
         raise Exception("Error: the __REPLACE_ANIMALS_INFO__ is not in the template.")
 
 
-def generate_animals_with_selected_skin_type(skin_type, animals_data):
+def collect_animals_with_selected_skin_type(skin_type, animals_data):
     """
-    Filter the animals by selected skin type.
+    Collects the animals by selected skin type from the animals JSON data.
     :param skin_type: Skin type as filter criteria.
     :param animals_data: JSON formatted animal data.
     :return: JSON formatted animal data filtered by a selected skin type.
@@ -191,32 +191,69 @@ def generate_animals_with_selected_skin_type(skin_type, animals_data):
     return filtered_animals, counter_animals_without_skin_type
 
 
+def print_skin_types(unique_skin_types):
+    """
+    Displays the available skin types or an error message if no skin types are available.
+    :param unique_skin_types:
+    :return: None
+    """
+    if len(unique_skin_types) == 0:
+        print("No skin types found in the animals data.")
+        return
+
+    print("Available skin types:")
+    for skin_type in unique_skin_types:
+        print(f"\t{skin_type}")
+
+
+def get_user_input(unique_skin_types):
+    """
+    Gets the selected skin type from the user input.
+    :param unique_skin_types: Unique skin types for all animals.
+    :return: Selected skin type by the user.
+    """
+    while True:
+        selected_skin_type = input("Select a skin type to use: ")
+        if selected_skin_type in unique_skin_types:
+            break;
+        print("Invalid skin type.")
+
+    return selected_skin_type
+
+
+def print_animals_without_skin_type(counter_animals_without_skin_type):
+    """
+    Displays the number of animals without skin type,
+    which could not participate in the filtering by the skin type.
+    :param counter_animals_without_skin_type: Number of animals without
+    skin type parameter in their data structure.
+    :return:
+    """
+    if counter_animals_without_skin_type > 0:
+        print(f"Attention! {counter_animals_without_skin_type} animals are not "
+              f"included in the animals data "
+              f"because of missing skin type data.")
+
+
 def main():
     try:
-        file_name = "animals.html"
+        # get JSON data
         animals_data = load_data("animals_data.json")
+
+        # manage the skin type
         unique_skin_types = get_unique_skin_types(animals_data)
+        print_skin_types(unique_skin_types)
+        selected_skin_type = get_user_input(unique_skin_types)
+        animals_with_selected_skin, counter_animals_without_skin_type = (
+            collect_animals_with_selected_skin_type(selected_skin_type, animals_data))
+        print_animals_without_skin_type(counter_animals_without_skin_type)
 
-        print("Available skin types:")
-        for skin_type in unique_skin_types:
-            print(f"\t{skin_type}")
-
-        while True:
-            selected_skin_type = input("Select a skin type to use: ")
-            if selected_skin_type in unique_skin_types:
-
-                break;
-            print("Invalid skin type.")
-
-        animals_with_selected_skin, counter_animals_without_skin_type = generate_animals_with_selected_skin_type(selected_skin_type, animals_data)
-
-        if counter_animals_without_skin_type > 0:
-            print(f"Attention! {counter_animals_without_skin_type} animals are not included in the animals data "
-                  f"because of missing skin type data.")
-
+        # manage the HTML
         html_formatted_animals = serialize_all_animals(animals_with_selected_skin)
         template_html_for_animals = get_template_html()
-        full_html_with_animals = insert_new_data_in_html_template(template_html_for_animals, html_formatted_animals)
+        full_html_with_animals = insert_new_data_in_html_template(
+            template_html_for_animals, html_formatted_animals)
+        file_name = "animals.html"
         create_html_file(file_name, full_html_with_animals)
         print(f"The website {file_name} was generated for animals with selected skin type.")
 
