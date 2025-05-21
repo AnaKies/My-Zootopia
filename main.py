56ace9c3d586a6e80fea1358ebd7ebbb83205d83
Anastasia Kiessig <anastasia.sibir@gmail.com>
2025-05-21 11:15:46 +0200

    Put animals string to a html file.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 
+from bs4 import BeautifulSoup
+
 
 def load_data(file_path):
     """
@@ -11,11 +13,11 @@ def load_data(file_path):
         return json.load(json_file)
 
 
-def print_animals(animals_data):
+def get_string_with_animals(animals_data):
     """
-    Prints the animal data.
-    :param animals_data: json formatted animal data.
-    :return: None
+    Gets the single string with predefined animal data.
+    :param animals_data: JSON formatted animal data.
+    :return: string with predefined animal data.
     """
     output_animals_data = ""
 
@@ -41,22 +43,24 @@ def print_animals(animals_data):
                 output_animals_data += f"Type: {type_of_animal}\n"
 
         output_animals_data += "\n"
-    print(output_animals_data)
+
+    return output_animals_data
 
 
 def get_html():
     """
     Reads the content of the HTML file.
-    :return: content of the HTML file.
+    :return: Content of the HTML file.
     """
     with open("animals_template.html", "r") as html_file:
         return html_file.read()
 
 
-
 def main():
     animals_data = load_data("animals_data.json")
-    print_animals(animals_data)
+    string_with_animals = get_string_with_animals(animals_data)
+    html_raw = get_html()
+    html_raw.replace("__REPLACE_ANIMALS_INFO__", string_with_animals)
 
 
 if __name__ == "__main__":
